22298773792c98a79ef90641b1378fdbd74912a5
Ahmed Amine Boustani <ahmed.a.boustani@gmail.com>
2017-06-07 13:22:33 +0000

    Bundling Module

---
+++ webpack.config.dev.js
@@ -0,0 +1,26 @@
+import path from 'path';
+
+export default {
+  debug: true,  // sends debugging info when runnign the build
+  devtool: 'inline-source-map',
+  noInfo: false,  // To remove the noise in the commandline
+  entry: [
+    path.resolve(__dirname, 'src/index')  // Application entry point
+  ],
+  target: 'web',  // could be node, electron....
+  // webpack won't generate any physical files for the development build
+  // it will create a bundle on memory and serve the build to the browser
+  output: {
+    path: path.resolve(__dirname, 'src'),  // where the app will start from
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    // The file types webpack should handle
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
