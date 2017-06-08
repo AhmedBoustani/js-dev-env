e6c7cdd9171eb78a80fdb50aa12f4ea4a4fd99d4
Ahmed Amine Boustani <ahmed.a.boustani@gmail.com>
2017-06-08 14:46:21 +0000

    Prod Deploy #1: Heroku

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -13,14 +13,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function(req, res){
-  res.json([
-    {"id": 1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
-    {"id": 2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@gmail.com"},
-    {"id": 3, "firstName":"tina", "lastName":"lee", "email":"lee.tina@gmail.com"},
-  ]);
-});
-
 app.listen(port, function (err) {
   if (err) {
 // eslint-disable-next-line no-console
